3073040c53840c071fbad39229d77baa0e6d9e01
Glad Chinda <gladxeqs@gmail.com>
2018-09-29 23:48:40 +0100

    Added test for random module.

---
+++ __tests__/random.js
@@ -0,0 +1,17 @@
+const random = require('../src/random');
+
+describe('random()', () => {
+  test('should return string of the given length containing only digits', () => {
+    const digits = random(6);
+    expect(typeof digits).toBe('string');
+    expect(digits).toHaveLength(6);
+    expect(digits).toMatch(/^\d+$/);
+  });
+
+  test('should return string containing only 1 digit when called without arguments', () => {
+    const digits = random();
+    expect(typeof digits).toBe('string');
+    expect(digits).toHaveLength(1);
+    expect(digits).toMatch(/^\d$/);
+  });
+});
